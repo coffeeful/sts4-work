5cb9505771131e4715e7a8fb13f92526e58bb7bb
coffeeful <dltjdgus03@gmail.com>
2024-03-13 16:06:16 +0900

    board

---
+++ thymeleafprj02/src/main/java/com/icia/thymeleafprj02/dto/ProductDto.java
@@ -0,0 +1,12 @@
+package com.icia.thymeleafprj02.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDto {
+	private String pname;
+	private String brand;
+	private int price;
+	private int amount;
+
+}
